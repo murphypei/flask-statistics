fcd4d98216a16e0bc4e5a44d88020b2c781a3304
murphy <murphypei@163.com>
2022-05-13 15:05:04 +0800

    split rtf>0 from normal

--- flask_statistics/utils.py
+++ flask_statistics/utils.py
@@ -28,9 +28,8 @@ class StatisticsQueries:
                                        func.count(self.model.path).label("hits"),
                                        func.count(self.model.remote_address.distinct()).label("unique_hits"),
                                        func.max(self.model.date).label("last_requested"),
-                                       func.avg(self.model.response_time).label("average_response_time")).group_by(
-                                           self.model.path).order_by(desc("hits")))
-
+                                       func.avg(self.model.response_time).label("average_response_time")).filter(
+                                           self.model.rtf <= 0).group_by(self.model.path).order_by(desc("hits")))
         query = self._add_date_filter_to_query(query, start_date, end_date)
 
         return query.all()
@@ -43,7 +42,6 @@ class StatisticsQueries:
                                        func.avg(self.model.response_time).label("average_response_time"),
                                        func.avg(self.model.rtf).label("average_rtf")).filter(
                                            self.model.rtf > 0).group_by(self.model.path).order_by(desc("hits")))
-
         query = self._add_date_filter_to_query(query, start_date, end_date)
 
         return query.all()
